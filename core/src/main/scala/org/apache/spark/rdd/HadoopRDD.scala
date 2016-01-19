95148b2081b3fa96c4a391b728f8e6d8024d7fdd
zx247549135 <zx247549135@gmail.com>
2016-01-19 20:32:57 +0800

    HadoopRDD may have some problems.

--- core/src/main/scala/org/apache/spark/rdd/HadoopRDD.scala
+++ core/src/main/scala/org/apache/spark/rdd/HadoopRDD.scala
@@ -231,20 +231,20 @@ class HadoopRDD[K, V](
       inputMetrics.setBytesReadCallback(bytesReadCallback)
 
       // ------add by MURS to get the bytes first
-      if(bytesReadCallback.isDefined)
-        inputMetrics.updateBytesRead()
-      else if (split.inputSplit.value.isInstanceOf[FileSplit] ||
-        split.inputSplit.value.isInstanceOf[CombineFileSplit]) {
-        // If we can't get the bytes read from the FS stats, fall back to the split size,
-        // which may be inaccurate.
-        try {
-          if(inputMetrics.bytesRead != 0L)
-            inputMetrics.incBytesRead(split.inputSplit.value.getLength)
-        } catch {
-          case e: java.io.IOException =>
-            logWarning("Unable to get input size to set InputMetrics for task", e)
-        }
-      }
+//      if(bytesReadCallback.isDefined)
+//        inputMetrics.updateBytesRead()
+//      else if (split.inputSplit.value.isInstanceOf[FileSplit] ||
+//        split.inputSplit.value.isInstanceOf[CombineFileSplit]) {
+//        // If we can't get the bytes read from the FS stats, fall back to the split size,
+//        // which may be inaccurate.
+//        try {
+//          if(inputMetrics.bytesRead != 0L)
+//            inputMetrics.incBytesRead(split.inputSplit.value.getLength)
+//        } catch {
+//          case e: java.io.IOException =>
+//            logWarning("Unable to get input size to set InputMetrics for task", e)
+//        }
+//      }
       // ------MURS----
 
       var reader: RecordReader[K, V] = null
